93b4d8e26a6bbfbfbc150bcb6e85e9476a8ecd94
imanchalsingh <singhanchal159357@gmail.com>
2025-12-24 14:44:29 +0530

    commit changes

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
